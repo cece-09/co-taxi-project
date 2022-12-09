03671a570e7f99646ab2dfae307ef3f61e834fd8
swhub021 <112057315+swhub021@users.noreply.github.com>
2022-12-09 11:53:23 +0900

    Add files via upload

---
+++ app/src/main/java/com/example/prj1114/SearchViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.prj1114
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel: ViewModel() {
+
+
+    var placeId: String? = null
+    var placeName: String? = null
+
+
+
+
+
+}
